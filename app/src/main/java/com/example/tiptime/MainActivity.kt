c188ce55f41ba8224396988dec2872308bfa5660
xPOGx <pavel19.1078@gmail.com>
2023-06-02 13:53:05 +0300

    New visual look for Views

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import java.util.Locale
@@ -17,16 +20,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
-        binding.tipOptions.setOnCheckedChangeListener { group, checkedId ->
-            if (checkedId == R.id.option_custom) {
-                binding.customPrice.visibility = View.VISIBLE
-            } else {
-                if (binding.customPrice.visibility == View.VISIBLE) {
-                    binding.customPrice.visibility = View.INVISIBLE
-                    binding.customPriceEditText.setText("")
-                }
-            }
-        }
+        binding.tipOptions.setOnCheckedChangeListener { _, checkedId -> showCustomEditView(checkedId) }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+        binding.customPriceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     private fun calculateTip() {
@@ -39,20 +36,21 @@ class MainActivity : AppCompatActivity() {
             R.id.option_twenty_percent -> 0.20
             R.id.option_fifteen_percent -> 0.15
             R.id.option_ten_percent -> 0.10
-            else -> {
-                val percent = binding.customPriceEditText.text.toString().toDoubleOrNull()
-                if (percent == null || percent < 1) {
-                    displayTip(0.0)
-                    return
-                }
-                percent / 100
-            }
+            else -> { customPercent() }
         }
         var tip = cost * tipPercentage
         if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
         displayTip(tip)
+    }
+
+    private fun customPercent(): Double {
+        val percent = binding.customPriceEditText.text.toString().toDoubleOrNull()
+        if (percent == null || percent < 1) {
+            return 0.0
+        }
+        return percent / 100
     }
 
     private fun displayTip(amount: Double) {
@@ -62,5 +60,27 @@ class MainActivity : AppCompatActivity() {
             val formattedTip = NumberFormat.getCurrencyInstance(Locale("uk", "UA")).format(amount)
             binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
         }
+    }
+
+    private fun showCustomEditView(checkedId: Int) {
+        if (checkedId == R.id.option_custom) {
+            binding.customPrice.visibility = View.VISIBLE
+        } else {
+            if (binding.customPrice.visibility == View.VISIBLE) {
+                binding.customPrice.visibility = View.INVISIBLE
+                binding.customPriceEditText.setText("")
+            }
+        }
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
